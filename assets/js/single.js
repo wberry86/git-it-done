530ff2f5d2d52a14cfa4b5959d0dd6d3009ec8d3
William Berry <76190311+wberry86@users.noreply.github.com>
2021-03-18 17:12:47 -0500

    added repo issues and warning

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function (repo) {
   var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -9,16 +10,35 @@ var getRepoIssues = function (repo) {
       response.json().then(function (data) {
         // pass response data to dom function
         displayIssues(data);
+
+        // check if api has paginated issues
+        if (response.headers.get("link")) {
+            console.log(displayWarning(repo));
+        }
       });
     } else {
       alert("There was a problem with your request!");
     }
+
   });
   console.log(repo);
 };
 
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+  linkEl.textContent = "See More Issues on GitHub.com";
+  linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+  linkEl.setAttribute("target", "_blank");
+
+  // append to warning container
+  limitWarningEl.appendChild(linkEl);
+};
+
 var displayIssues = function (issues) {
-    
+
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
